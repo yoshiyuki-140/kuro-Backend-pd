7554aefa813abcb8892702c5cd323e951ed51bd6
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-07-04 19:11:09 +0900

    src/requestsProxyTest.pyの文法エラーを修正した

--- src/requestsProxyTest.py
+++ src/requestsProxyTest.py
@@ -2,7 +2,7 @@ import requests
 import URLs
 
 proxies = {
-    'http':'http://wwwproxykanazawa-it.ac.jp:8080'
+    'http':'http://wwwproxykanazawa-it.ac.jp:8080',
     'https':'https://wwwproxykanazawa-it.ac.jp:8080'
 }
 
