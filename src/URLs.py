27e691c95b779b89e60ac672d6310a11ec015bf8
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-22 00:16:11 +0900

    I created project folder

--- src/URLs.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-# Author : Yoshiyuki Kurose
-
-"""
-    必要なURLを変数に格納しているだけのデータベース
-"""
-
-AllprojectURL = "https://kanazawa.ha4go.net/projects"
-BaseURL = "https://kanazawa.ha4go.net"
--- src/requestsProxyTest.py
+++
@@ -1,9 +0,0 @@
-import requests
-import URLs
-
-proxies = {
-    'http':'http://wwwproxykanazawa-it.ac.jp:8080',
-    'https':'https://wwwproxykanazawa-it.ac.jp:8080'
-}
-
-res = requests(URLs.AllprojectURL,proxies=proxies)
