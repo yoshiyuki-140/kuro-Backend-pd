186f15dbc5a0ae68caf81511e9e443a0a9e530b4
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-28 01:23:50 +0900

    reset project

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.views import LoginView,LogoutView
-from django.urls import path
-
-urlpatterns = [
-    path('login/',LoginView.as_view(redirect_authenticated_user=True,template_name='accounts/login.html'),name='login'),
-    path('logout/',LogoutView.as_view(template_name='accounts/logout.html'),name='logout'),
-]
-
