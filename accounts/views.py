b7707d97f8537023677c0e7cb12f9c700aae4dad
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-29 21:42:00 +0900

    I added login.html and Login function which is default django function

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from accounts.models import Accounts
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import AccountsCreationForm
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = AccountsCreationForm(request.POST)
+        if form.is_valid():
+            accounts_data = form.save(commit=False)
+            accounts_data.created_by = request.user
+            # ここになんかredirect先の関数を書かないといけないとらしい.
+            # return redirect()
+
+    else:
+        form = AccountsCreationForm()
+    return render(request,'accounts/signup.html',{'form':form})
+
+@login_required
+def signin(request,user_id):
+    pass
--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'accounts.Accounts'
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = '/'
