2f46368b643224fe28f442cc16a01a8fe250734b
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-01 22:08:26 +0900

    init all apps

--- accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import Accounts
-
-class AccountsCreationForm(UserCreationForm):
-    class Meta:
-        model = Accounts
-        fields = ['name', 'birth_data', 'email', 'password','region','profession']
--- accounts/views.py
+++
@@ -1,6 +0,0 @@
-from accounts.models import Accounts
-from django.shortcuts import render,redirect,get_object_or_404
-from django.contrib.auth.decorators import login_required
-from .forms import AccountsCreationForm
-
-# Create your views here.
