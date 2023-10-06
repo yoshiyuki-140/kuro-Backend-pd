cf3b6ca00b9f60ebfef28a4d38d9ca6b56686efa
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-06 17:06:11 +0900

    I added Tags models

--- topics/models.py
+++ topics/models.py
@@ -7,8 +7,15 @@ class Topics(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     creted_by = models.ForeignKey('auth.User', related_name='topics', on_delete=models.CASCADE)
+    like_count = models.IntegerField(default=0)
 
 class Comments(models.Model):
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey('auth.User', related_name='comments', on_delete=models.CASCADE)
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('auth.User', related_name='tags', on_delete=models.CASCADE)
