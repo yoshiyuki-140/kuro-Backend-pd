46cab5963d872e584d351ef23df6eab611edc554
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-06 22:03:17 +0900

    I added bugFix and created topic's database models

--- topics/models.py
+++ topics/models.py
@@ -1,21 +1,52 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
+class Tags(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "tags"
+
+    def __str__(self):
+        return self.name
+
 class Topics(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    creted_by = models.ForeignKey('auth.User', related_name='topics', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name="タイトル",max_length=100)
+    description = models.TextField(verbose_name="概要")
+    created_at = models.DateTimeField("投稿日",auto_now_add=True)
+    # models.PROTECTをしていすることで投稿者が消えてもトピックを消さないようにする.
+    creted_by = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                                  related_name='topics_account', 
+                                  verbose_name="投稿者",
+                                  on_delete=models.PROTECT,
+                                  default="",
+                                  blank=True)
+    tags = models.ManyToManyField(Tags,
+                                  verbose_name='タグ',
+                                  blank=True)
     like_count = models.IntegerField(default=0)
+    class Meta:
+        db_table = "topics"
+
+    def __str__(self):
+        return self.title
 
 class Comments(models.Model):
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey('auth.User', related_name='comments', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                                   related_name='comments_account', 
+                                   verbose_name="投稿者",
+                                   on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topics, 
+                              related_name='topics', 
+                              verbose_name="トピック",
+                              on_delete=models.CASCADE)
 
+    class Meta:
+        db_table = "comments"
 
-class Tags(models.Model):
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey('auth.User', related_name='tags', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.comment
+
