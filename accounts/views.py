9a16017d7ffb3c4f7072917c43332f3641c781a1
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-12 20:18:02 +0900

    I modified accounts's views.py for fix 'NoReverseMatch at /accounts/signup/' error

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,7 @@ class SignupView(CreateView):
     app_name = "accounts"
     form_class = UserCreationForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("accounts:accounts/signup")
+    success_url = reverse_lazy("accounts:signup")
 
     def form_valid(self, form: BaseModelForm):
         user = form.save()
