616b0a4ea2a1a92c7cf89a33631aaedef65bf209
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-12 20:29:59 +0900

    I fixed 'ImportError: cannot import name 'topic_new' from 'topics.views' (C:\Users\moyas\Documents\programmes\apps\CivicSeek\topics\views.py)' error

--- topics/urls.py
+++ topics/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-from .views import topic_new,topic_edit,topic_detail
+from .views import DetailTopicView,EditTopicView,NewTopicView
 
 # postリクエストを受け取る用のviewルーティング
 from .views import comment_new
 
 app_name = "topics"
 
+topic_new = NewTopicView.as_view()
+topic_edit = EditTopicView.as_view()
+topic_detail = DetailTopicView.as_view()
+
 urlpatterns = [
     path('topic_new/',topic_new,name='topic_new'),
-    path('topic_edit/',topic_edit,name='topic_edit'),
-    path('topic_detail/',topic_detail,name='topic_detail'),
+    path('<int:pk>/edit',topic_edit,name='topic_edit'),
+    path('<int:pk>/',topic_detail,name='topic_detail'),
     path('comment_new/',comment_new,name='comment_new'),
 ]
