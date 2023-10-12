f18906837305282b677ac9f4912dc0ef7f8792d2
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-12 19:47:49 +0900

    I changed topic's Views from function based view to Classbased view with the exception of comments's Model

--- topics/forms.py
+++ topics/forms.py
@@ -1,6 +1,11 @@
 from django.forms import ModelForm
 from topics.models import Topics,Comments
 
+# ここではModelFormクラスを継承してフォームを作成しているけど
+# 今後細かい設定をフォームに対して行うことがあると思うからその時は
+# from django.forms import Formを実行し、Formの継承を行うこと
+# Formクラスを使用したフォームの設定方法に関しては以下のURLが参考になるだろう
+# https://office54.net/python/django/forms-field-argument
 
 class TopicForm(ModelForm):
     class Meta:
--- topics/views.py
+++ topics/views.py
@@ -4,65 +4,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseForbidden
 from .forms import TopicForm,CommentForm
 from .models import Topics,Comments
-
-# Create your views here.
-
-
-@login_required
-def topic_new(request):
-    if request.method == 'POST':
-        form = TopicForm(request.POST)
-        if form.is_valid():
-            topic = form.save(commit=False)
-            topic.created_by = request.user
-            topic.save()
-            messages.add_message(request, messages.SUCCESS,
-                                 "トピックを作成しました。")
-            return redirect(topic_detail, topic_id=topic.pk)
-        else:
-            messages.add_message(request, messages.ERROR,
-                                 "トピックの作成に失敗しました。")
-    else:
-
-        form = TopicForm()
-    return render(request, "topics/topic_detail.html", {'form': form})
-
-
-@login_required
-def topic_edit(request, topic_id):
-    topic = get_object_or_404(Topics, pk=topic_id)
-    # ここにworningを示す赤波線が出るのは
-    if topic.create_by_id != request.user.id:
-        return HttpResponseForbidden("このトピックの編集は許可されていません。")
-
-    if request.method == "POST":
-        form = TopicForm(request.POST, instance=topic)
-        if form.is_valid():
-            form.save()
-            messages.add_message(request, messages.SUCCESS,
-                                 "トピックを更新しました。")
-            return redirect('topic_detail', topic_id=topic_id)
-        else:
-            messages.add_message(request, messages.ERROR,
-                                 "トピックの更新に失敗しました。")
-    else:
-        form = TopicForm(instance=topic)
-    return render(request, 'topics/topic_edit.html', {'form': form})
-
-
-@login_required
-def topic_detail(request, topic_id):
-    topic = get_object_or_404(Topics, pk=topic_id)
-    comments = Comments.objects.filter(commented_to=topic_id).all()
-    comment_form = CommentForm()
-
-    return render(request, 
-        "topic_detail.html", 
-        {
-        'topic': topic,
-        'comments': comments,
-        'comment_form': comment_form,
-        })
+from django.views.generic import *
+# LoginRequiredMixin：クラスベースビューをログインが確認できないと表示できないようにする継承用クラス
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 @login_required
 def comment_new(request, topic_id):
@@ -80,3 +24,50 @@ def comment_new(request, topic_id):
         messages.add_message(request, messages.ERROR,
                              "コメントの投稿に失敗しました。")
     return redirect('topics', topic_id=topic_id)
+
+# create class base view
+from django.contrib import messages
+from django.shortcuts import resolve_url
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import TopicForm
+from .models import Topics
+
+class NewTopicView(CreateView,LoginRequiredMixin):
+    model = Topics
+    form_class = TopicForm
+    # 投稿に成功した時のURL
+    # ここでは実験的にトップページに飛ぶようにする
+    success_url = reverse_lazy('general:top') # このコードを書いた時点ではtopはgeneralに属していないので動かない可能性が高い
+
+    
+    def get_success_url(self) -> str:
+        '''
+        投稿に成功した際に実行される処理を書くメソッド
+        This method will execute, when success getting url.
+        '''
+        successMsg = "課題を投稿しました"
+        messages.success(self.request,successMsg)
+        return resolve_url('general:top')
+
+class EditTopicView(UpdateView,LoginRequiredMixin):
+    """
+    CivicSeekにおいて課題をupdateするときに使うview要素
+    """
+    model = Topics
+    # 表示する入力フォームの属性
+    fields = ('title','description')
+    # テンプレートファイルの拡張子を取り除いたものの最後につく名前ここでは"form_edit.html"を指定したいから"_edit"を指定した.
+    template_name_suffix = "_edit"
+    # updateが完了した時のリダイレクト先の名前,
+    # ここでいう「名前」とは,path関数のname属性で指定されるものと等しい意味を持つ
+    success_url = reverse_lazy('topic_detail')
+
+class DetailTopicView(DetailView,LoginRequiredMixin):
+    """課題の詳細を表示するページを作成するview.
+
+    Args:
+        DetailView (_type_): _description_
+    """
+    model = Topics
