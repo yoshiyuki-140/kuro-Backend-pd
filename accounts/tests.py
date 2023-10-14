c31122d421f41ab52c3ee290d6deb6b539d1f4e2
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-15 08:05:16 +0900

    compelte

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class SignUpTest(TestCase):
+    def test_do_contain_specified_text(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertContains(response,"会員登録",status_code=200)
+
+    def test_is_use_template(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertTemplateUsed(response,'accounts/signup.html')
+
