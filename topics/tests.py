c7fde752aad17d034b443ae60379acbabdfa11af
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-10-16 12:15:40 +0900

    I fixed creating topic bug

--- topics/tests.py
+++ topics/tests.py
@@ -1,3 +1,72 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.contrib.auth import get_user_model
+from topics.models import Topics,Tags,Comments
+
+UserModel = get_user_model()
+
+class NewTopicTemplateTest(TestCase):
+    def testShouldReturn200statuscodeAndContainSpecifiedString(self):
+        """'課題投稿'という文字列をstatuscode200で返してきたレスポンスに含まれているかテスト
+        """
+        response = self.client.get('/topics/topic_new/')
+        self.assertContains(response,'課題投稿',status_code=200)
+
+    def testShouldUseSpecifiedTemplate(self):
+        """指定したテンプレートを使用しているかどうかテスト
+        """
+        response = self.client.get('/topics/topic_new/')
+        self.assertTemplateUsed(response,'topics/topic_new.html')
+
+class EditTopicTemplateTest(TestCase):
+    """'topic_new.html'を使用できるかテスト
+    """
+
+    def setUp(self):
+        """テストでのみ用いるデータの作成 : テストが終わったら消される (・∀・)
+        このORMコードが正しいことはデバッグ済み. -> def test(self)を作成しその中に書いた
+        """
+        self.user = UserModel.objects.create(
+            username='test_user',
+            email='test@example.com',
+            password='secret',
+        )
+
+        self.tag = Tags.objects.create(
+            name = 'test tag',
+        )
+
+        self.topic = Topics.objects.create(
+            title = 'test topic',
+            description = 'test description',
+            created_by = self.user, # ここはself.user.idにしたほうがいいのかも
+            # tags = self.tag.pk, # ここもself.tag.idのほうがいいかも
+            like_count = 11,
+        )
+
+        self.topic.tags.add(self.tag)
+        self.topic.created_by = self.user
+
+        self.comment = Comments.objects.create(
+            comment = 'test comment',
+            created_by = self.user,
+            commented_to = self.topic,
+        )
+        self.location = f'/topics/{self.user.pk}/edit/'
+        self.client.force_login(self.user)
+
+    def testShouldReturn200statuscodeAndContainSpecifiedString(self):
+        """'課題編集'という文字列がstatuscode200で返してきたレスポンスに含まれているかテスト
+        """
+        response = self.client.get(self.location)
+        self.assertContains(response,'課題編集',status_code=200)
+
+    def testShouldUseSpecifiedTemplate(self):
+        """指定したテンプレートを使用しているかどうかテスト
+        """
+        response = self.client.get(self.location)
+        self.assertTemplateUsed(response,'topics/topics_form.html')
+
+
--- topics/urls.py
+++ topics/urls.py
@@ -1,18 +1,21 @@
 from django.urls import path
-from .views import DetailTopicView,EditTopicView,NewTopicView
+from .views import DetailTopicView,EditTopicView,NewTopicView,ListTopicView
 
 # postリクエストを受け取る用のviewルーティング
-from .views import comment_new
+from .views import NewCommentView
 
 app_name = "topics"
 
 topic_new = NewTopicView.as_view()
 topic_edit = EditTopicView.as_view()
 topic_detail = DetailTopicView.as_view()
+topic_list = ListTopicView.as_view()
+comment_new = NewCommentView.as_view()
 
 urlpatterns = [
     path('topic_new/',topic_new,name='topic_new'),
-    path('<int:pk>/edit',topic_edit,name='topic_edit'),
+    path('<int:pk>/edit/',topic_edit,name='topic_edit'),
     path('<int:pk>/',topic_detail,name='topic_detail'),
-    path('comment_new/',comment_new,name='comment_new'),
+    path('<int:pk>/comment_new/',comment_new,name='comment_new'),
+    # path('topic_list/',topic_list,name='topic_list'),
 ]
