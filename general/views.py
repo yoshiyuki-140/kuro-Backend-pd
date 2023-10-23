d6f9702d54e606f2bf60f7de8bc0b19f5ff9f58f
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-10-24 01:07:19 +0900

    updated get_context_date method in general app's views.py. because, I must be update got db at topic was created

--- general/views.py
+++ general/views.py
@@ -10,12 +10,12 @@ class ToppageView(TemplateView):
     template_name = 'general/top.html'
     # ここでget_object_or_404を使用してTopicsのテーブルオブジェクトを指定するとtopicのデータテーブルを参照してしまいマイグレーション前に 
     # "such table not found" エラーが出る
-    topic = Topics.objects.all()
     
     # コンテキストに何を入れるのかを追加で指定する
     # get_context_dataのオーバーライドをしているので
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
+        self.topic = Topics.objects.all()
         context["topics"] = self.topic
         return context
     
