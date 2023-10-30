1cc95756e191abcef15eb1e3c28502d6ba428bff
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-10-30 20:07:16 +0900

    I added topics models

--- topics/models.py
+++ topics/models.py
@@ -1,3 +1,56 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.conf import settings
+# Create your models here.
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "tags"
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    title = models.CharField(verbose_name="タイトル",max_length=100)
+    description = models.TextField(verbose_name="概要")
+    created_at = models.DateTimeField("投稿日",auto_now_add=True)
+    # models.PROTECTをしていすると投稿者が消えてもトピックを消さないようにできる.
+    # ここではmodels.CASCADEを指定し、投稿者のアカウントが消えると投稿も消えるようにする
+    created_by = models.ForeignKey(UserModel,
+                                  related_name='topics_account', 
+                                  verbose_name="投稿者",
+                                  on_delete=models.CASCADE,
+                                  default=None,
+                                  blank=True)
+    tags = models.ManyToManyField(Tag,
+                                  verbose_name='タグ') # ManyToManyはデフォでblank=Trueになってる
+    like_count = models.IntegerField(default=0)
+    class Meta:
+        db_table = "topics"
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    comment = models.TextField()
+    # 下のcreated_atはauto_now_add=Trueにしてしまうと,編集された時も更新されるため、変数名的に適切ではない.から消した
+    created_at = models.DateTimeField()
+    created_by = models.ForeignKey(UserModel,
+                                   related_name='comments_account', 
+                                   verbose_name="投稿者",
+                                   on_delete=models.CASCADE)
+    commented_to = models.ForeignKey(Topic, 
+                              related_name='topics', 
+                              verbose_name="トピック",
+                              on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "comments"
+
+    def __str__(self):
+        return self.comment
