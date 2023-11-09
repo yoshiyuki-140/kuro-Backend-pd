35fdf9e93025ec0950bda506d2b667adb5cbe01d
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-11-10 05:43:44 +0900

    I compelted profile windows

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,23 +7,35 @@ app_name = "accounts"
 
 signup_view = SignupView.as_view(template_name='accounts/signup.html')
 login_view = LoginView.as_view(template_name='accounts/login.html')
-logout_view = LogoutView.as_view(template_name='accounts/logout.html',redirect_field_name='top')
-password_change_view = PasswordChangeView.as_view(template_name='accounts/password_change.html')
-password_change_done_view = PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html')
-password_reset_view = PasswordResetView.as_view(template_name='accounts/password_reset.html')
-password_reset_done_view = PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html')
-password_reset_confirm_view = PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html')
-password_reset_complete_view = PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html')
+logout_view = LogoutView.as_view(
+    template_name='accounts/logout.html', redirect_field_name='top')
+password_change_view = PasswordChangeView.as_view(
+    template_name='accounts/password_change.html')
+password_change_done_view = PasswordChangeDoneView.as_view(
+    template_name='accounts/password_change_done.html')
+password_reset_view = PasswordResetView.as_view(
+    template_name='accounts/password_reset.html')
+password_reset_done_view = PasswordResetDoneView.as_view(
+    template_name='accounts/password_reset_done.html')
+password_reset_confirm_view = PasswordResetConfirmView.as_view(
+    template_name='accounts/password_reset_confirm.html')
+password_reset_complete_view = PasswordResetCompleteView.as_view(
+    template_name='accounts/password_reset_complete.html')
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('password_change/', password_change_view, name='password_change'),
-    path('password_change/done/', password_change_done_view,name='password_change_done'),
+    path('password_change/done/', password_change_done_view,
+         name='password_change_done'),
     path('password_reset/', password_reset_view, name='password_reset'),
-    path('password_reset/done/', password_reset_done_view, name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', password_reset_confirm_view, name='password_reset_confirm'),
-    path('reset/done/', password_reset_complete_view,name='password_reset_complete'),
-    path('signup_success/',signup_success,name='signup_success'),
+    path('password_reset/done/', password_reset_done_view,
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', password_reset_confirm_view,
+         name='password_reset_confirm'),
+    path('reset/done/', password_reset_complete_view,
+         name='password_reset_complete'),
+    path('signup_success/', signup_success, name='signup_success'),
     path('signup/', signup_view, name='signup'),
+    path('profile/', profile_view, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,8 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LogoutView
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
 
 # Create your views here.
 
@@ -16,7 +17,8 @@ def signup_success(request):
     '''アカウント
     '''
     template_name = 'accounts/signup_success.html'
-    return render(request,template_name)
+    return render(request, template_name)
+
 
 class SignupView(CreateView):
     app_name = "accounts"
@@ -24,22 +26,32 @@ class SignupView(CreateView):
     template_name = "accounts/signup.html"
     success_url = reverse_lazy('home')
 
-
     def form_valid(self, form: BaseModelForm):
         user = form.save()
         login(self.request, user)
-        messages.add_message(self.request, messages.SUCCESS,"ユーザー登録しました。")
+        messages.add_message(self.request, messages.SUCCESS, "ユーザー登録しました。")
         self.object = user
         # return HttpResponseRedirect(self.get_success_url())
-        return render(request=self.request,template_name='accounts/signup_success.html')
+        return render(request=self.request, template_name='accounts/signup_success.html')
 
-    def form_invalid(self
-                     , form):
-        messages.add_message(self.request, messages.ERROR,"ユーザー登録に失敗しました。")
+    def form_invalid(self, form):
+        messages.add_message(self.request, messages.ERROR, "ユーザー登録に失敗しました。")
         return super().form_invalid(form)
+
 
 class CustomLogoutView(LogoutView):
     # ログアウト後のリダイレクト先を指定するためのget_success_urlメソッドをオーバーライドします。
     def get_success_url(self):
         # ログアウト後にリダイレクトしたいURLを指定します。
         return reverse_lazy('home')  # このURLをカスタマイズしてください
+
+
+def profile_view(request):
+    '''profile
+    '''
+    if request.method == "POST":
+        '''編集ボタンが押されたら
+        '''
+        return redirect('')
+
+    return render(request, 'accounts/profile.html', {'user': request.user})
